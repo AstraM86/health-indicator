4415d6112686295c6abf6caecdf101272670baf5
Vera <kuprivm@mail.ru>
2026-02-07 21:28:16 +0300

    correction

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
     node: true,
-    jest: true
   },
   extends: ['eslint:recommended'],
-  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ['@babel/preset-env']
-    }
   },
   rules: {
-    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
-  plugins: ['jest'],
-  overrides: [
-    {
-      files: ['**/__tests__/**/*.js', '**/*.test.js'],
-      rules: {
-        'no-undef': 'off'
-      }
-    }
-  ]
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'coverage/',
+    '*.log',
+    'webpack.config.js',
+    'babel.config.json'
+  ],
 };
